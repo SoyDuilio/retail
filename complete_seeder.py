6c2586c70c673c9e1f74caa5080319d156c23fb4
SoyDuilio <cramadalsuarez@gmail.com>
2025-09-27 16:16:46 -0500

    Update complete_seeder.py
    ddd

--- complete_seeder.py
+++ complete_seeder.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:duilia@localhost:5432/pedidos_db"
+    raise Exception("DATABASE_URL requerida")
 
 engine = create_engine(DATABASE_URL)
 
